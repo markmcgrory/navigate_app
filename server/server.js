2df608c09f8b7ede3cfab06a1e6e883b0efe9803
Mark McGrory <mark_mcgrory@hotmail.co.uk>
2023-09-30 11:51:50 +0100

    seeds populated

--- server/server.js
+++ server/server.js
@@ -1,1 +1,23 @@
-console.log("Hello world");
+const express = require('express');
+const app = express();
+
+const cors = require('cors');
+app.use(cors());
+
+const MongoClient = require('mongodb').MongoClient;
+const createRouter = require('./helpers/create_router.js');
+
+app.use(express.json());
+
+MongoClient.connect('mongodb://127.0.0.1:27017', { useUnifiedTopology: true })
+  .then((client) => {
+    const db = client.db('navigate');
+    const serviceUsersCollection = db.collection('serviceUsers');
+    const serviceUsersRouter = createRouter(serviceUsersCollection);
+    app.use('/api/service-users', serviceUsersRouter);
+  })
+  .catch(console.err);
+
+app.listen(9000, function () {
+  console.log(`Listening on port ${ this.address().port }`);
+});
