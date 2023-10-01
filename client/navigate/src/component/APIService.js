da75a4de31f62a88945d8880d151e75c020df770
Mark McGrory <mark_mcgrory@hotmail.co.uk>
2023-10-01 11:08:32 +0100

    Created Services data in the Navigate Container

---
+++ client/navigate/src/component/APIService.js
@@ -0,0 +1,38 @@
+import React from 'react';
+const baseURL = 'http://localhost:9000/api/service-users'
+
+const APIService = {
+    getUsers() {
+        return fetch(baseURL)
+        .then(res => res.json())
+    },
+
+    postUser(payload) {
+        return fetch(baseURL, {
+            method: 'POST',
+            body: JSON.stringify(payload),
+            headers: { 'Content-Type': 'application/json'}
+        })
+        .then(res => res.json())
+    },
+    
+    deleteUser(id) {
+        return fetch(baseURL + id, {
+            method: 'DELETE'
+        })
+    },
+
+    updateUser(user) {
+        return fetch(baseURL + user._id, {
+            method: 'PUT',
+            body: JSON.stringify(user),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(res => res.json())
+    }
+
+}
+ 
+export default APIService;
--- client/navigate/src/container/NavigateContainer.js
+++ client/navigate/src/container/NavigateContainer.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import NavBar from '../component/NavBar';
 import Footer from '../component/Footer';
 
 const NavigateContainer = () => {
+
+    const ServicesData = [
+        {name: "Aspire Paisley", location: "Paisley Road West"},
+        {name:"Aspire Queen Margaret", location: "Queen Margaret Drive"},
+        {name:"Aspire Copeland", location: "Coperland Road Hotel"},
+        {name: "Help the Homeless Glasgow", location:"45b Gartcraig Road, PO Box 2274, Glasgow, G33 2WX"},
+        {name: "Simon Community Kent", location: "Kent Road"},
+        {name:"Simon Community Maxwell", location:"Maxwell Drive"},
+        {name:"Homeless Casework Team", location:"30 Mansion Street, Glasgow G22 5SZ"},
+        {name:"Shelter Scotland", location:"116 Osborne Street, Glasgow"},
+        {name:"Glasgow city mission", location:"20 Crimea Street, Glasgow G2 8PW"}
+    ]
+
+    const [services, setServices] = useState(ServicesData)
+
     return (
         <div>
             <NavBar/>
