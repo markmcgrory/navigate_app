d021e6ca2779fa80bd2575d2987a288f4da564c8
Mark McGrory <mark_mcgrory@hotmail.co.uk>
2023-10-01 13:08:11 +0100

    nav bar created

--- client/navigate/src/component/NavBar.js
+++ client/navigate/src/component/NavBar.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import styled from 'styled-components';
+
+const NavBarStyle = styled.div`
+display: flex;
+justify-content: space-evenly;
+align-items: center;
+`
+
 
 const NavBar = () => {
     return (
-        <div>
-            <h1>I'm a NavBar</h1>
+        <NavBarStyle>
             <Link to='/'>Home</Link>
             <Link to='/service-users'>Service Users</Link>
             <Link to='/services'>Services</Link>
-        </div>
+        </NavBarStyle>
       );
 }
  
